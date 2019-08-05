215bd4a310dce7010e87dca272b186f3ef339032
sway0308 <sway0308@gmail.com>
2019-08-05 14:05:57 +0800

    Bt 檔案重新命名處理器

---
+++ BtFileFinder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace BtFileProcesserNet
+{
+    public class BtFileFinder
+    {
+        /// <summary>
+        /// 重新命名影音檔案
+        /// </summary>
+        /// <param name="rootPath">欲搜尋的根目錄</param>
+        /// <param name="extName">影音檔副檔名</param>
+        public void RenameVideoFile(string rootPath, string extName)
+        {
+            var result = new Dictionary<string, string>();
+            var dirs = Directory.EnumerateDirectories(rootPath);
+            foreach (var dir in dirs)
+            {
+                var ans = from m in Directory.EnumerateFiles(dir, $"*.{extName}")
+                          from j in Directory.EnumerateFiles(dir, "*.jpg")
+                          where m.Substring(0, m.Length - 4) != j.Substring(0, j.Length - 4)
+                          select new
+                          {
+                              RealName = j.Substring(0, j.Length - 4),
+                              FilePath = Path.Combine(dir, m)
+                          };
+                ans.ToList().ForEach(x => result.Add(x.FilePath, x.RealName));
+            }
+
+            foreach (var filePath in result.Keys)
+            {
+                var path = Path.GetDirectoryName(filePath);
+                var realPath = Path.Combine(path, $"{result[filePath]}.{extName}");
+                File.Move(filePath, realPath);
+            }
+        }
+
+        /// <summary>
+        /// 重新命名資料夾名稱
+        /// </summary>
+        /// <param name="rootPath">欲搜尋的根目錄</param>
+        public void RenameDirName(string rootPath)
+        {
+            var result = new Dictionary<string, string>();
+            var dirs = Directory.EnumerateDirectories(rootPath);
+            foreach (var dir in dirs)
+            {
+                var ans = from j in Directory.EnumerateFiles(dir, "*.jpg")
+                          where dir != j.Substring(0, j.Length - 4)
+                          select new
+                          {
+                              RealName = j.Substring(0, j.Length - 4),
+                              Path = dir
+                          };
+                ans.ToList().ForEach(x => result.Add(x.Path, x.RealName));
+            }
+
+            foreach (var path in result.Keys)
+            {
+                var parentPath = Path.GetPathRoot(path);
+                var realPath = Path.Combine(parentPath, $"{result[path]}");
+                Directory.Move(path, realPath);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtFileProcesserNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0: RenameVideoFile, 1: RenameDirName");
+            var option = Console.ReadLine();
+            if (option != "0" && option != "1")
+            {
+                Console.WriteLine("Bye");
+                Console.ReadLine();
+            }
+
+            var finder = new BtFileFinder();
+            if (option == "0")
+            {
+                Console.WriteLine("RootPath:");
+                var rootPath = Console.ReadLine();
+                Console.WriteLine("Ext Name");
+                var extName = Console.ReadLine();
+                finder.RenameVideoFile(rootPath, extName);
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+
+            if (option == "1")
+            {
+                Console.WriteLine("RootPath:");
+                var rootPath = Console.ReadLine();
+                finder.RenameDirName(rootPath);
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+        }
+    }
+}
