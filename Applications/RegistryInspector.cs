ac7566a63ad7fdb86b41540a6d0f89514661a030
sway0308 <sway0308@gmail.com>
2019-08-07 17:29:39 +0800

    加入觀測機碼並刪除的console

---
+++ Applications/RegistryInspector.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BtFileProcesserNet
+{
+    class RegistryInspector
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer(3000);
+            timer.Elapsed += (s, e) => {
+                Console.WriteLine(DateTime.Now);
+                DeleteRegistryKey();
+                Console.ReadLine();
+            };
+            Console.WriteLine("Start timer");
+            timer.Start();
+            HandleConsole();
+        }
+
+        private static void HandleConsole()
+        {
+            var result = Console.ReadLine();
+            if (result.ToLower() != "exit")
+                HandleConsole();
+        }
+
+        private static void DeleteRegistryKey()
+        {
+            //讀取Registry Key位置
+            using (var RegK = Registry.LocalMachine.OpenSubKey(@"Software\Policies\Google\Chrome", RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                var value = RegK.GetValue("RemoteAccessHostFirewallTraversal")?.ToString();
+                if (value != null)
+                {
+                    Console.WriteLine($"Registry Value = {value}");
+                    Console.WriteLine($"Start delete registry");
+                    RegK.DeleteValue("RemoteAccessHostFirewallTraversal", true);
+                    Console.WriteLine($"finish delete registry");
+                }
+                else
+                {
+                    Console.WriteLine("No Registry");
+                }
+            }
+        }
+    }
+}
